cbe38d3f0d34f70e8851e28e4d21c2ded9884aa2
MET <mustafatpe23@gmail.com>
2021-04-20 00:18:40 +0300

    Btk part 3

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,43 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public IResult Add(Product product)
+        {
+            if (product.CategoryId==1)
+            {
+                return new ErrorResult("Bu kategoride ürün kaydı yapılmamaktadır");
+            }
+            else
+            {
+                _productDal.Add(product);
+                return new SuccessResult("Ürün eklendi");
+            }
+        }
+
+        public IDataResult<List<Product>> GetAll()
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),"Ürünler Listelendi");
+        }
+
+        public IDataResult<List<Product>> GetAllByCategory(int categoryId)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId==categoryId));
+        }
+    }
+}
