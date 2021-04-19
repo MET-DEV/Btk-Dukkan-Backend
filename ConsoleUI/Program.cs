be7f08ca62ca3e549daa8381b2db1da93ee6a781
MET <mustafatpe23@gmail.com>
2021-04-19 23:16:32 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EfProductDal efProductDal = new EfProductDal();
+            Product product1 = new Product { Name ="Su",BrandId=1,CategoryId=1,Price=1,CreateDate=DateTime.Now,Code="WTR0",Active=true };
+            //efProductDal.Add(product1);
+
+            //foreach (var product in efProductDal.GetAll())
+            //{
+            //    Console.WriteLine(product.Name);
+            //}
+        }
+
+        
+
+       
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=BtkDukkan;Trusted_Connection=true");
+        }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
